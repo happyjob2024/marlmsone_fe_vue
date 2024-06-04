0159e2485c7d7f2c9f979b713485c8c4369a68e7
sungjin <sungjine456@gmail.com>
2024-06-04 17:01:30 +0900

    #43 취업정보 페이지 구현

---
+++ src/components/employ/StudentList.vue
@@ -0,0 +1,177 @@
+<script setup>
+import Pagination from '@/components/common/PaginationComponent.vue';
+import SaveEmployModal from '@/components/modals/employ/SaveEmployModal.vue';
+import { Employ } from '@/api/api';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const emptyEmploy = {
+    action: 'I',
+    id: 0,
+    name: '',
+    company: '',
+    tel: '',
+    startDate: '',
+    endDate: '',
+};
+
+const emit = defineEmits(['doAction']);
+
+const pageSize = 5;
+const currentPage = ref(1);
+const totalCount = ref(0);
+
+const saveEmploy = ref(emptyEmploy);
+const saveModalFlag = ref(false);
+
+const searchValue = ref('');
+const searchKey = ref('all1');
+const list = ref([]);
+
+const searchList = (page) => {
+    page = page || 1;
+
+    const params = new URLSearchParams();
+    params.append('currentPage', page);
+    params.append('pageSize', pageSize);
+    params.append('searchInfo1', searchValue.value);
+    params.append('searchKey1', searchKey.value);
+
+    axios
+        .post(Employ.GetStudents, params)
+        .then((res) => {
+            list.value = res.data.stdList;
+            totalCount.value = res.data.std_Total;
+            currentPage.value = page;
+        })
+        .catch((_) => {
+            alert('실패했습니다.');
+        });
+};
+
+const openSaveModal = (employ) => {
+    saveEmploy.value = {
+        action: 'I',
+        id: employ.std_id,
+        name: employ.name,
+        company: employ.comp_name,
+        tel: employ.tel,
+        startDate: employ.employ_day,
+        endDate: employ.resign_day,
+    };
+    saveModalFlag.value = true;
+};
+
+const closeSaveModal = (data) => {
+    saveEmploy.value = emptyEmploy;
+    saveModalFlag.value = false;
+
+    if (data) {
+        emit('doAction');
+        searchList();
+    }
+};
+
+defineExpose({ searchList });
+
+onMounted(() => {
+    searchList();
+});
+</script>
+
+<template>
+    <div style="margin-top: 10px">
+        <p class="conTitle">
+            <span>학생 정보</span>
+            <span class="search">
+                <select class="form-select" v-model="searchKey">
+                    <option value="all1">전체</option>
+                    <option value="t_name">학생명</option>
+                    <option value="lec_nm">강의명</option>
+                </select>
+                <input type="text" class="form-control" v-model="searchValue" />
+                <button class="btn btn-primary" @click="searchList()">
+                    검색
+                </button>
+            </span>
+        </p>
+        <table class="col">
+            <colgroup>
+                <col width="10%" />
+                <col width="10%" />
+                <col width="20%" />
+                <col width="20%" />
+                <col width="20%" />
+                <col width="10%" />
+            </colgroup>
+            <thead>
+                <tr>
+                    <th scope="col">학번</th>
+                    <th scope="col">학생명</th>
+                    <th scope="col">연락처</th>
+                    <th scope="col">강의명</th>
+                    <th scope="col">가입일자</th>
+                    <th scope="col">등록</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-if="!list.length">
+                    <td colSpan="6">해당되는 데이터가 없습니다.</td>
+                </tr>
+                <tr v-for="item in list" :key="item.std_id">
+                    <td>{{ item.std_num }}</td>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.tel }}</td>
+                    <td>{{ item.lec_name }}</td>
+                    <td>{{ item.join_date }}</td>
+                    <td>
+                        <button
+                            class="btn btn-primary"
+                            @click="openSaveModal(item)"
+                        >
+                            등록
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <Pagination
+            v-bind="{
+                currentPage,
+                totalItems: totalCount,
+                itemsPerPage: pageSize,
+            }"
+            @search="searchList($event)"
+            v-if="list.length > 0"
+            style="margin: 15px 0 30px 0"
+        />
+        <SaveEmployModal
+            v-if="saveModalFlag"
+            :info="saveEmploy"
+            @closeModal="closeSaveModal"
+        />
+    </div>
+</template>
+
+<style scoped>
+.search {
+    display: flex;
+    align-items: center;
+}
+
+.search select {
+    width: 90px;
+    height: auto;
+    padding-left: 5px;
+}
+
+.search input {
+    width: 200px;
+    margin-right: 5px;
+}
+
+.btn {
+    padding: 6px;
+    font-size: 15px;
+}
+</style>
---
+++ src/views/employ/EmploymentInfo.vue
@@ -0,0 +1,221 @@
+<script setup>
+import Pagination from '@/components/common/PaginationComponent.vue';
+import StudentList from '@/components/employ/StudentList.vue';
+import SaveEmployModal from '@/components/modals/employ/SaveEmployModal.vue';
+import { Employ } from '@/api/api';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const emptyEmploy = {
+    action: 'U',
+    id: 0,
+    name: '',
+    company: '',
+    tel: '',
+    startDate: '',
+    endDate: '',
+};
+
+const pageSize = 5;
+const currentPage = ref(1);
+const totalCount = ref(0);
+
+const searchValue = ref('');
+const searchKey = ref('all');
+const list = ref([]);
+
+const studentListRef = ref();
+const updateEmploy = ref(emptyEmploy);
+const updateModalFlag = ref(false);
+
+const searchList = (page) => {
+    page = page || 1;
+
+    const params = new URLSearchParams();
+    params.append('currentPage', page);
+    params.append('pageSize', pageSize);
+    params.append('searchInfo', searchValue.value);
+    params.append('searchKey', searchKey.value);
+
+    axios
+        .post(Employ.GetList, params)
+        .then((res) => {
+            list.value = res.data.empInfo;
+            totalCount.value = res.data.emp_Total;
+            currentPage.value = page;
+        })
+        .catch((_) => {
+            alert('목록을 불러오는데 실패했습니다.');
+        });
+};
+
+const openUpdateModal = (employ) => {
+    updateEmploy.value = {
+        action: 'U',
+        id: employ.std_id,
+        name: employ.name,
+        company: employ.comp_name,
+        tel: employ.tel,
+        startDate: employ.employ_day,
+        endDate: employ.resign_day,
+    };
+    updateModalFlag.value = true;
+};
+
+const closeUpdateModal = (data) => {
+    updateEmploy.value = emptyEmploy;
+    updateModalFlag.value = false;
+
+    if (data) {
+        list.value = list.value.map((i) => {
+            if (i.std_id === data.id) {
+                return {
+                    ...i,
+                    comp_name: data.company,
+                    employ_day: data.startDate,
+                    resign_day: data.endDate,
+                    wp_state: data.endDate ? '퇴사' : '재직중',
+                };
+            } else {
+                return i;
+            }
+        });
+    }
+};
+
+const deleteInfo = (id) => {
+    if (window.confirm('정말 삭제하시겠습니까?')) {
+        axios
+            .post(Employ.Delete, null, { params: { std_id: id } })
+            .then((res) => {
+                alert(res.data.resultMsg);
+                searchList(list.value.length === 1 ? 1 : currentPage.value);
+                studentListRef.value.searchList();
+            })
+            .catch((_) => {
+                alert('삭제에 실패했습니다.');
+            });
+    }
+};
+
+onMounted(() => {
+    searchList();
+});
+</script>
+<template>
+    <div class="content">
+        <p class="Location">
+            <a href="#" class="btn_set home">메인으로</a>
+            <span class="btn_nav bold">취업관리 / 취업 정보</span>
+            <a href="#" class="btn_set refresh">새로고침</a>
+        </p>
+        <p class="conTitle">
+            <span>취업목록</span>
+            <span class="search">
+                <select class="form-select" v-model="searchKey">
+                    <option value="all">전체</option>
+                    <option value="name">학생명</option>
+                    <option value="comp_name">회사명</option>
+                </select>
+                <input type="text" class="form-control" v-model="searchValue" />
+                <button class="btn btn-primary" @click="searchList()">
+                    검색
+                </button>
+            </span>
+        </p>
+        <div>
+            <table class="col">
+                <colgroup>
+                    <col width="7%" />
+                    <col width="10%" />
+                    <col width="12%" />
+                    <col width="10%" />
+                    <col width="10%" />
+                    <col width="28%" />
+                    <col width="10%" />
+                    <col width="13%" />
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th scope="col">학번</th>
+                        <th scope="col">학생명</th>
+                        <th scope="col">연락처</th>
+                        <th scope="col">입사일</th>
+                        <th scope="col">퇴사일</th>
+                        <th scope="col">회사명</th>
+                        <th scope="col">재직여부</th>
+                        <th scope="col">수정/삭제</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-if="!list.length">
+                        <td colSpan="8">해당되는 데이터가 없습니다.</td>
+                    </tr>
+                    <tr v-for="item in list" :key="item.std_id">
+                        <td>{{ item.std_num }}</td>
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.tel }}</td>
+                        <td>{{ item.employ_day }}</td>
+                        <td>{{ item.resign_day }}</td>
+                        <td>{{ item.comp_name }}</td>
+                        <td>{{ item.wp_state }}</td>
+                        <td>
+                            <button
+                                class="btn btn-primary"
+                                @click="openUpdateModal(item)"
+                            >
+                                수정
+                            </button>
+                            /
+                            <button
+                                class="btn btn-primary"
+                                @click="deleteInfo(item.std_id)"
+                            >
+                                삭제
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Pagination
+                v-bind="{
+                    currentPage,
+                    totalItems: totalCount,
+                    itemsPerPage: pageSize,
+                }"
+                @search="search($event)"
+                v-if="list.length > 0"
+                style="margin: 15px 0 30px 0"
+            />
+        </div>
+        <StudentList ref="studentListRef" @doAction="searchList()" />
+        <SaveEmployModal
+            v-if="updateModalFlag"
+            :info="updateEmploy"
+            @closeModal="closeUpdateModal"
+        />
+    </div>
+</template>
+
+<style scoped>
+.search {
+    display: flex;
+    align-items: center;
+}
+
+.search select {
+    width: 90px;
+    height: auto;
+    padding-left: 5px;
+}
+
+.search input {
+    width: 200px;
+    margin-right: 5px;
+}
+
+.btn {
+    padding: 6px;
+    font-size: 15px;
+}
+</style>
