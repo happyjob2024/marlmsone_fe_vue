b486cc5764949e3db1c8ef5f99ca71ffed03345a
jinnyBee <hjjeon81@gmail.com>
2024-06-04 11:48:35 +0900

    수강인원 modal-body 사이즈 조정

--- src/views/adm/ModalCouseSize.vue
+++ src/views/adm/ModalCouseSize.vue
@@ -3,20 +3,20 @@
         <div class="backdrop">
             <div class="container">
                 <div class="modal-dialog">
-                    <div class="modal-content" style="width: 500px; margin: 0 auto; overflow-y: auto;">
+                    <div class="modal-content" >
                         <div class="modal-header">
                             <p class="conTitle">
                                 <span>수강인원 상세보기</span>
                             </p>
                         </div>
-                        <div class="modal-body">
+                        <div class="modal-body" style="width: 500px; height: 550px;">
                             <!-- {"totalCnt": 27,
                                 "lecList": [{"lec_id": 1,"tutor_id": null,"lec_name": "자바의이해","max_pnum": 51,"pre_pnum": 0,
                                     "start_date": "2024.03.05","end_date": "2024.06.15","process_day": null,"test_id": 0,
                                     "loginID": null,"user_type": null,"use_yn": null,"name": "스티븐잡스","std_id": null,
                                     "test_score": 0,"tot_score": 286,"max_score": 100,"min_score": 43,"avg_score": 71,
                                     "fail_cnt": 2,"fail_rate": 0.0}],"pageSize": 6,"currentPage": 1} -->
-                            <table class="table table-bordered ">
+                            <table class="table table-bordered" >
                                 <colgroup>
                                     <col width="25%">
                                     <col width="25%">
